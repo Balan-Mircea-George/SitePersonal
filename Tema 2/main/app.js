20e68522f1102f292e2bc7cde18ebae97a03166b
Bălan Mircea-George <101660706+Balan-Mircea-George@users.noreply.github.com>
2022-11-16 01:14:13 +0200

    Add files via upload

---
+++ Tema 2/main/app.js
@@ -0,0 +1,26 @@
+function addTokens(input, tokens){
+    
+	if(typeof input !== 'string'){
+		throw new Error('Invalid input');
+	}
+	if(input.length<6){
+		throw new Error('Input should have at least 6 characters');
+	}
+	for(let i in tokens){ 
+	if(typeof tokens[0].tokenName !== 'string'){
+		throw new Error('Invalid array format');
+	}}
+	if(input.search(/[...]/)==-1){
+		return input;
+	}
+	for(let i in tokens){ 
+	input = input.replace('...', "${" + tokens[i].tokenName + "}");
+	}
+	return input;
+}
+
+const app = {
+    addTokens: addTokens
+}
+
+module.exports = app;
